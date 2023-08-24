aae3e6a0a5d547db7f50b8ef6b54d38ab57e53cb
Szymon Kokot <60388922+azerty0220pl@users.noreply.github.com>
2023-08-24 21:30:56 +0200

    Rooms dynamic

--- rooms.php
+++ rooms.php
@@ -4,9 +4,16 @@ require_once('./config.php');
 
 use eftec\bladeone\BladeOne;
 
+$page = (int)htmlspecialchars($_GET["page"]);
+$sql = "SELECT * FROM rooms ORDER BY id LIMIT 9 OFFSET " . 9 * $page . ";";
+$result = $conn->query($sql);
+$count_res = $conn->query("SELECT COUNT('id') as 'count' FROM rooms");
+
+$count_res = ($count_res->fetch_assoc())['count'];
+
 $views = __DIR__ . "/views";
 $cache = __DIR__ . "/cache";
 
 $blade = new BladeOne($views, $cache, BladeOne::MODE_DEBUG);
-echo $blade->run('rooms');
+echo $blade->run('rooms', ["rooms" => $result, "count" => $count_res, "page" => $page]);
 ?>
